bb06b4bb297d607f1b42329df02aeb4542767d5a
okjy89 <okjt89@hanyang.ac.kr>
2025-05-17 02:28:16 +0900

    only_class_remain

--- MlpNetwork.py
+++ MlpNetwork.py
@@ -2,10 +2,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-C = 32
-H = W = 32
-roi_feat = torch.randn(1, C, H, W)  # (B, C, H, W)
-
 class KernelPredictor(nn.Module):
     def __init__(self, C):
         super().__init__()
@@ -23,8 +19,3 @@ class KernelPredictor(nn.Module):
         weights = self.mlp(x)
         weights = weights.view(B, self.C, self.C, 3, 3)
         return weights
-
-# 커널 추론 및 적용
-predictor = KernelPredictor(C)
-kernel = predictor(roi_feat)[0]  # shape: [C_out, C_in, 3, 3]
-enhanced = F.conv2d(roi_feat, kernel, padding=1)
