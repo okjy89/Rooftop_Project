98949735c6304a976d5955eeb63e44add8d1eb69
okjy89 <okjt89@hanyang.ac.kr>
2025-05-30 00:08:34 +0900

    Add 1-channel 3x3 kernel generation code

--- MlpNetwork.py
+++ MlpNetwork.py
@@ -19,3 +19,22 @@ class KernelPredictor(nn.Module):
         weights = self.mlp(x)
         weights = weights.view(B, self.C, self.C, 3, 3)
         return weights
+    
+
+
+
+class KernelPredictor2(nn.Module):
+    def __init__(self, Cin):
+        super().__init__()
+        self.mlp = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),   # [B, C, 1, 1]
+            nn.Flatten(),              # [B, C]
+            nn.Linear(Cin, 128),
+            nn.ReLU(),
+            nn.Linear(128, 9),         # → 3x3 = 9개 weight
+        )
+
+    def forward(self, x):
+        kernel = self.mlp(x)           # [B, 9]
+        kernel = kernel.view(-1, 1, 3, 3)  # → [B, 1, 3, 3]
+        return kernel
